112df5c4054e206908acab67dbc5f74af0061abd
Jose Angel Silva <jasilva@grupo-orsa.com.mx>
2023-04-24 21:02:07 -0600

    Add: Cap 04 Criteria API
    Se crea consultas de JPQL con parametros adicionales
    se crea el funcionamiento de API criteria de JPA

--- src/main/java/com/jasilva/hibernatejpa/dao/ProductoDao.java
+++ src/main/java/com/jasilva/hibernatejpa/dao/ProductoDao.java
@@ -1,9 +1,18 @@
 package com.jasilva.hibernatejpa.dao;
 
 import com.jasilva.hibernatejpa.modelo.Productos;
+
+import java.time.LocalDate;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+
 import static org.hibernate.criterion.Expression.sql;
 
 /**
@@ -53,5 +62,43 @@ public class ProductoDao {
 
     public Double consultarPrecioProducto(String nombre){
         return em.createNamedQuery("Producto.consultaPrecio", Double.class).setParameter("nombre", nombre).getSingleResult();
+    }
+
+    // consultas con parametros dinamicos
+    public List<Productos> consultaPorParametros(String nombre, Double precio, LocalDate fecha){
+        StringBuilder jpql =new StringBuilder("select p from Productos p where  1=1 ");
+        if(nombre != null && !nombre.trim().isEmpty())
+            jpql.append("and p.nombre =:nombre ");
+        if(precio != null && !precio.equals(0))
+            jpql.append("and p.precio =:precio ");
+        if(fecha != null)
+            jpql.append("and p.fechaRegistro =:fecha ");
+        TypedQuery<Productos> query = em.createQuery(jpql.toString(), Productos.class);
+        if(nombre != null && !nombre.trim().isEmpty())
+            query.setParameter("nombre", nombre);
+        if(precio != null && !precio.equals(0))
+            query.setParameter("precio", precio);
+        if(fecha != null)
+            query.setParameter("fecha", fecha);
+
+        return query.getResultList();
+    }
+    //consulta por parametros dinamicos con API Criteria
+    public List<Productos> consultaDinamicaCriteria(String nombre, Double precio, LocalDate fecha){
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Productos> query = builder.createQuery(Productos.class);
+        Root<Productos> from = query.from(Productos.class);
+
+        Predicate predicate = builder.and();
+
+        if(nombre != null && !nombre.trim().isEmpty())
+            predicate = builder.and(predicate,builder.equal(from.get("nombre"),nombre));
+        if(precio != null && !precio.equals(0))
+            predicate = builder.and(predicate,builder.equal(from.get("precio"),precio));
+        if(fecha != null)
+            predicate = builder.and(predicate,builder.equal(from.get("fechaRegistro"),fecha));
+
+        query = query.where(predicate);
+        return em.createQuery(query).getResultList();
     }
 }
--- src/main/java/com/jasilva/hibernatejpa/test/PruebaDesempeno.java
+++ src/main/java/com/jasilva/hibernatejpa/test/PruebaDesempeno.java
@@ -1,10 +1,14 @@
 package com.jasilva.hibernatejpa.test;
 
 import com.jasilva.hibernatejpa.dao.PedidoDAO;
+import com.jasilva.hibernatejpa.dao.ProductoDao;
 import com.jasilva.hibernatejpa.modelo.Pedido;
+import com.jasilva.hibernatejpa.modelo.Productos;
 import com.jasilva.hibernatejpa.utils.JPAUtils;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.List;
 
 public class PruebaDesempeno {
     public static void main(String[] args) {
@@ -12,9 +16,21 @@ public class PruebaDesempeno {
         EntityManager em = JPAUtils.getEntityManager();
 
         PedidoDAO pedido = new PedidoDAO(em);
+        ProductoDao prod = new ProductoDao(em);
         Pedido pedidoCliente = pedido.consultarPedidoCliente(1);
+        LocalDate fechaHoy =  LocalDate.now();
+        var productos = prod.consultaPorParametros(null,null, fechaHoy);
+        var productos2 = prod.consultaDinamicaCriteria(null,null, null);
         em.close();
 
         System.out.println(pedidoCliente.getCliente().getNombre());
+        for (Productos pro:
+             productos) {
+            System.out.println(pro.getNombre() + " - " + pro.getDescripcion());
+        }
+        System.out.println("--------------------------------");
+        for (Productos pro: productos2) {
+            System.out.println(pro.getNombre() + " - " + pro.getDescripcion());
+        }
     }
 }
